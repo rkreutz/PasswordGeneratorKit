221d8530c487145ab2a63084c5514f2d79b60685
Rodrigo Kreutz <rkreutz>
2020-06-17 23:28:49 +0100

    Adding methods to use default character sets

--- Sources/PasswordGeneratorKit/PasswordRule/PasswordRule+Defaults.swift
+++ Sources/PasswordGeneratorKit/PasswordRule/PasswordRule+Defaults.swift
@@ -9,5 +9,25 @@ public extension PasswordRule {
         .mustContain(characterSet: .symbolCharacters, atLeast: 1)
     ])
 
+    static func mustContainLowercaseCharacters(atLeast count: Int) -> PasswordRule {
+
+        .mustContain(characterSet: .lowercaseCharacters, atLeast: count)
+    }
+
+    static func mustContainUppercaseCharacters(atLeast count: Int) -> PasswordRule {
+
+        .mustContain(characterSet: .uppercaseCharacters, atLeast: count)
+    }
+
+    static func mustContainDecimalCharacters(atLeast count: Int) -> PasswordRule {
+
+        .mustContain(characterSet: .decimalCharacters, atLeast: count)
+    }
+
+    static func mustContainSymbolCharacters(atLeast count: Int) -> PasswordRule {
+
+        .mustContain(characterSet: .symbolCharacters, atLeast: count)
+    }
+
     static let defaultRules = defaultCharacterSet.union([defaultLength])
 }
